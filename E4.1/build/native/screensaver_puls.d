73197e23de21dac2deaa1393a00e7f29a57fca98
241220000-lzh <2473212358@qq.com>
2026-03-31 22:23:08 +0800

    rm

--- E4.1/build/native/screensaver_puls.d
+++
@@ -1,6 +0,0 @@
-/home/lzh/.ssh/ysyx-workbench/PA1/build/native/screensaver_puls.o: \
- /home/lzh/.ssh/ysyx-workbench/PA1/screensaver_puls.c \
- /home/lzh/.ssh/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/lzh/.ssh/ysyx-workbench/abstract-machine/am/include/arch/native.h \
- /home/lzh/.ssh/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/lzh/.ssh/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
